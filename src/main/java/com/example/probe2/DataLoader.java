1bf47932003b58fc9ae37795158e3842cc43e8fd
IbrahimAbduqani <adgn.deut@gmail.com>
2022-11-20 00:32:27 +0100

    second

--- src/main/java/com/example/probe2/DataLoader.java
+++ src/main/java/com/example/probe2/DataLoader.java
@@ -1,2 +1,36 @@
-package com.example.probe2;public class DataLoader {
+package com.example.probe2;
+import com.example.probe2.TodoItem;
+import com.example.probe2.TodoItemReposetory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.List;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+    @Autowired
+    private TodoItemReposetory todoItemRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        List<TodoItem> todos = (List<TodoItem>) todoItemRepository.findAll();
+
+        if (todos.size() == 0) {
+            TodoItem todo1 = new TodoItem();
+            TodoItem todo2 = new TodoItem();
+
+            todo1.setDescription("this is the first todo");
+            todo1.setCreatedDate(Instant.now());
+
+            todo2.setDescription("This is the second todo");
+            todo2.setCreatedDate(Instant.now());
+
+            todoItemRepository.save(todo1);
+            todoItemRepository.save(todo2);
+
+
+        }
+    }
 }
--- src/main/java/com/example/probe2/TodoItemReposetory.java
+++ src/main/java/com/example/probe2/TodoItemReposetory.java
@@ -1,2 +1,8 @@
-package com.example.probe2;public interface TodoItemReposetory {
+package com.example.probe2;
+import com.example.probe2.TodoItem;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoItemReposetory extends CrudRepository<TodoItem, Long> {
 }
